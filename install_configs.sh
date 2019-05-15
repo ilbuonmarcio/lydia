3b3ef7e907b84a428de09b342e0d76c9530cc8c0
maaaybe <marciozgaming@gmail.com>
2019-05-15 22:05:29 +0200

    Added mkdir on folders

--- install_configs.sh
+++ install_configs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+mkdir ../../.config/i3        > /dev/null
+mkdir ../../.config/alacritty > /dev/null
+mkdir ../../.config/polybar   > /dev/null
 cp -rf ./configs/i3/*         ../../.config/i3/
 cp -rf ./configs/alacritty/*  ../../.config/alacritty/
 cp -rf ./configs/polybar/*    ../../.config/polybar/
