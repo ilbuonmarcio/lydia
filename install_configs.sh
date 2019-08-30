fa2418463bb2347716accc5fa249e3805f8c5c1c
maaaybe <marciozgaming@gmail.com>
2019-08-30 23:11:59 +0200

    Moved from alacritty to kitty

--- install_configs.sh
+++ install_configs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 mkdir ../../.scripts          2> /dev/null
 mkdir ../../.config/i3        2> /dev/null
-mkdir ../../.config/alacritty 2> /dev/null
+mkdir ../../.config/kitty 2> /dev/null
 mkdir ../../.config/polybar   2> /dev/null
 mkdir ../../.config/gtk-3.0/  2> /dev/null
 cp -rf ./configs/i3/*         ../../.config/i3/
-cp -rf ./configs/alacritty/*  ../../.config/alacritty/
+cp -rf ./configs/kitty/*  ../../.config/kitty/
 cp -rf ./configs/polybar/*    ../../.config/polybar/
 cp -rf ./configs/zsh/.zshrc   ../../
 cp -rf ./configs/Xresources/.Xresources ../../
