b9b0945cde30ffebea2d38048c7254feefcd1a9e
maaaybe <marciozgaming@gmail.com>
2019-08-31 21:11:03 +0200

    Added code-oss settings

--- install_configs.sh
+++ install_configs.sh
@@ -5,6 +5,7 @@ mkdir ../../.config/kitty 2> /dev/null
 mkdir ../../.config/polybar   2> /dev/null
 mkdir ../../.config/gtk-3.0/  2> /dev/null
 mkdir ../../.config/compton/  2> /dev/null
+mkdir ../../.config/Code\ -\ OSS/User/ 2> /dev/null
 cp -rf ./configs/i3/*         ../../.config/i3/
 cp -rf ./configs/kitty/*  ../../.config/kitty/
 cp -rf ./configs/polybar/*    ../../.config/polybar/
@@ -14,3 +15,4 @@ cp -rf ./configs/Xresources/.Xresources ../../
 cp -rf ./configs/vim/.vimrc ../../
 cp -rf ./configs/gtk3/settings.ini ../../.config/gtk-3.0/
 cp -rf ./scripts/* ../../.scripts/
+cp -rf ./configs/code/settings.json ../../.config/Code\ -\ OSS/User/
