4a02372485e4170a410a27430a153f4b6a7f80a0
maaaybe <marciozgaming@gmail.com>
2019-08-31 13:06:29 +0200

    Project re-theme

--- scripts/toggle_polybar.sh
+++ scripts/toggle_polybar.sh
@@ -2,7 +2,8 @@
 
 echo 
 if [ $(pidof polybar | wc -l) = 0 ]; then
-	polybar -q example &>/dev/null &
+	polybar -q -c=$HOME/.config/polybar/topbar topbar &>/dev/null &
+	polybar -q -c=$HOME/.config/polybar/dock dock &>/dev/null &
 else
 	kill -9 $(pidof polybar)
 fi
