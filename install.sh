93a74ca9ddc193b92cc6e424166c1df40c5f3cbe
maaaybe <marciozgaming@gmail.com>
2019-09-07 13:06:10 +0200

    Added trials for setting passwords

--- install.sh
+++ install.sh
@@ -113,15 +113,15 @@ arch-chroot /mnt sed -ie 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NO
 arch-chroot /mnt mkinitcpio -p linux
 
 # setting root password
-echo "Insert password for root:"
-arch-chroot /mnt passwd
+echo "Insert password for root: "
+arch-chroot /mnt sudo -u root /bin/zsh -c "while [[ 'NP' == $(awk '{print $2}' <(sudo passwd -S root)) ]] do; passwd; done;"
 
 # making user mrcz
 arch-chroot /mnt useradd -m -G wheel -s /bin/zsh mrcz
 
 # setting mrcz password
 echo "Insert password for mrcz:"
-arch-chroot /mnt passwd mrcz
+arch-chroot /mnt sudo -u root /bin/zsh -c "while [[ 'NP' == $(awk '{print $2}' <(sudo passwd -S mrcz)) ]] do; passwd mrcz; done;"
 
 # installing grub bootloader
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot  --bootloader-id=GRUB --removable
