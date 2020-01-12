a95506928cbb0adefee3f5db205dd123a4c95e4f
ilbuonmarcio <marciozgaming@gmail.com>
2020-01-12 20:18:44 +0100

    Removed dockbar thanks to @zetaemme

--- scripts/toggle_polybar.sh
+++ scripts/toggle_polybar.sh
@@ -3,7 +3,6 @@
 echo 
 if [ $(pidof polybar | wc -l) = 0 ]; then
 	polybar -q -c=$HOME/.config/polybar/topbar topbar &>/dev/null &
-	polybar -q -c=$HOME/.config/polybar/dock dock &>/dev/null &
 else
 	kill -9 $(pidof polybar)
 fi
