0645434a1d64a05b1276f1075610995c2db49b12
ilbuonmarcio <marciozgaming@gmail.com>
2020-01-18 01:56:37 +0100

    Removed some unused packages

--- install.sh
+++ install.sh
@@ -82,11 +82,12 @@ pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux-headers \
 os-prober efibootmgr ntfs-3g kitty git zsh intel-ucode cpupower xf86-video-amdgpu vlc \
 xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
 chromium firefox code xf86-video-intel zip unzip unrar obs-studio docker \
-pulseaudio pasystray pamixer telegram-desktop go python python-pip wget openssh xorg-xrandr noto-fonts-emoji \
-maim imagemagick xclip pinta light ranger ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom \
-hsetroot docker-compose nemo linux-firmware firewalld tree man glances ttf-cascadia-code darktable fzf \
-mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau \
-zsh-syntax-highlighting xdotool cronie dunst entr
+pulseaudio pasystray pamixer telegram-desktop go python python-pip wget \
+openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light ranger \
+ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom hsetroot docker-compose \
+nemo linux-firmware firewalld tree man glances ttf-cascadia-code darktable fzf \
+mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver \
+mesa-vdpau lib32-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr
 
 # generating fstab
 genfstab -U /mnt >> /mnt/etc/fstab
