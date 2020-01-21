016f73a1fb464036ee0b4900ef213c867543b8f2
ilbuonmarcio <marciozgaming@gmail.com>
2020-01-21 23:50:10 +0100

    Added display setup changes for new RX 5700 XT build

--- scripts/setup_multimonitor_screens.sh
+++ scripts/setup_multimonitor_screens.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144 --primary
-xrandr --output HDMI-A-0 --mode 2560x1440 --rate 75 --left-of DisplayPort-0
+xrandr --output DisplayPort-2 --mode 2560x1440 --rate 144 --primary
+xrandr --output DisplayPort-1 --mode 2560x1440 --rate 75 --left-of DisplayPort-2
