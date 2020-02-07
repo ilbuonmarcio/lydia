493b772a028e003b7ec06b4e8100ff56470b230e
ilbuonmarcio <marciozgaming@gmail.com>
2020-02-07 01:06:09 +0100

    Swapped displays on setup multimonitor script

--- scripts/setup_multimonitor_screens.sh
+++ scripts/setup_multimonitor_screens.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-xrandr --output DisplayPort-2 --mode 2560x1440 --rate 144 --primary
-xrandr --output DisplayPort-1 --mode 2560x1440 --rate 75 --left-of DisplayPort-2
+xrandr --output DisplayPort-1 --mode 2560x1440 --rate 144 --primary
+xrandr --output DisplayPort-2 --mode 2560x1440 --rate 75 --left-of DisplayPort-1
