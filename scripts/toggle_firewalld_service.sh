dd478fcbb335cd4174985143b5ed44358c010b94
ilbuonmarcio <marciozgaming@gmail.com>
2020-02-24 18:55:14 +0100

    Added dunstify messages to firewalld polybar module

--- scripts/toggle_firewalld_service.sh
+++ scripts/toggle_firewalld_service.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
 if [[ "$(systemctl is-active firewalld.service)" == "active" ]]; then
+    dunstify --timeout=2000 --urgency=CRITICAL "Shutting down firewall..."
     sudo systemctl stop firewalld.service
+    dunstify --timeout=2000 --urgency=CRITICAL "Firewall shut down!"
 else
+    dunstify --timeout=2000 --urgency=CRITICAL "Starting firewall..."
     sudo systemctl start firewalld.service
+    dunstify --timeout=2000 --urgency=CRITICAL "Firewall started!"
 fi
