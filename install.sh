d5886948433ae1a2d94609779c08289c511406e7
ilbuonmarcio <marciozgaming@gmail.com>
2020-03-14 02:08:33 +0100

    Removed mandatory exit on oh-my-zsh install

--- install.sh
+++ install.sh
@@ -236,7 +236,7 @@ arch-chroot /mnt sudo -u mrcz yay -S xflux --noconfirm
 arch-chroot /mnt sudo -u mrcz /bin/zsh -c "yes | yay -S freetype2-infinality-remix fontconfig-infinality-remix cairo-infinality-remix"
 
 # installing oh-my-zsh
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+arch-chroot /mnt sudo -u mrcz curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh && chmod +x install.sh && RUNZSH=no ./install.sh && rm ./install.sh
 
 # installing pi theme for zsh
 arch-chroot /mnt sudo -u mrcz /bin/zsh -c "wget -O /home/mrcz/.oh-my-zsh/themes/pi.zsh-theme https://raw.githubusercontent.com/tobyjamesthomas/pi/master/pi.zsh-theme"
