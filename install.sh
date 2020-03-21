9103cd95eb7321ce6ed75cf5f4041e083a083c80
ilbuonmarcio <marciozgaming@gmail.com>
2020-03-21 08:27:48 +0100

    Added geckodriver

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ mount /dev/${selected_disk}2 /mnt/home
 pacstrap /mnt base base-devel vim networkmanager rofi feh linux linux-headers \
 os-prober efibootmgr ntfs-3g alacritty git zsh intel-ucode amd-ucode cpupower xf86-video-amdgpu vlc \
 xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
-chromium firefox code xf86-video-intel zip unzip unrar obs-studio docker \
+chromium firefox geckodriver code xf86-video-intel zip unzip unrar obs-studio docker \
 pulseaudio mate-media pamixer telegram-desktop go python python-pip wget nginx mariadb \
 openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light ranger \
 ttf-roboto playerctl papirus-icon-theme hwloc p7zip hsetroot docker-compose \
