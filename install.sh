baa560aa2abad3f9d61a7f0918ebd9d6e1a24176
ilbuonmarcio <marciozgaming@gmail.com>
2020-03-24 12:34:55 +0100

    Added gucharmap

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ mount /dev/${selected_disk}2 /mnt/home
 # pacstrap-ping desired disk
 pacstrap /mnt base base-devel vim networkmanager rofi feh linux linux-headers \
 os-prober efibootmgr ntfs-3g alacritty git zsh intel-ucode amd-ucode cpupower xf86-video-amdgpu vlc \
-xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
+xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts gucharmap \
 chromium firefox geckodriver code xf86-video-intel zip unzip unrar obs-studio docker \
 pulseaudio pasystray pamixer telegram-desktop go python python-pip wget nginx mariadb \
 openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light ranger \
