4fedbe7da9b2aa8ec05e74384a239a300b0c55f9
ilbuonmarcio <marciozgaming@gmail.com>
2020-03-26 14:31:13 +0100

    Added infinality ttf patches

--- install.sh
+++ install.sh
@@ -115,6 +115,7 @@ arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 arch-chroot /mnt ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+arch-chroot /mnt zsh -c `echo 'export FREETYPE_PROPERTIES="truetype:interpreter-version=38"' >> /etc/profile.d/freetype2.sh`
 
 # synchronizing timer
 arch-chroot /mnt hwclock --systohc
