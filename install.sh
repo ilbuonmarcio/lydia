9504c06d5aabf3a9967f7dffdef41c54aab7e4d7
GiacomoTezza <tezza.giacomo01@gmail.com>
2020-05-02 01:06:49 +0200

    Added proper changes to make it mine

--- install.sh
+++ install.sh
@@ -1,17 +1,3 @@
-echo "";
-echo "                              _       _        ";
-echo "       _ __ ___  _ __ ___ ___| |_ __ | | _____ ";
-echo "      | '_ \` _ \| '__/ __|_  / | '_ \| |/ / __|";
-echo "      | | | | | | | | (__ / /| | | | |   <\__ \\";
-echo "      |_| |_| |_|_|  \___/___|_|_| |_|_|\_\___/";
-echo "                                        ";
-echo "                                                       ";
-
-echo "     Easy-to-configure archlinux+i3 install script ";
-echo "        for maximum comfort and minimum hassles ";
-echo "";
-echo "";
-
 root_password="123"
 user_password="123"
 
@@ -19,7 +5,7 @@ user_password="123"
 boot_partition_size=500
 
 # home partition size, in GB
-home_partition_size=40
+home_partition_size=380
 
 # checks wheter there is multilib repo enabled properly or not
 IS_MULTILIB_REPO_DISABLED=$(cat /etc/pacman.conf | grep "#\[multilib\]" | wc -l)
@@ -93,11 +79,11 @@ os-prober efibootmgr ntfs-3g kitty git zsh amd-ucode cpupower xf86-video-amdgpu 
 xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts gucharmap \
 firefox geckodriver code zip unzip unrar obs-studio adapta-gtk-theme \
 pulseaudio pasystray pamixer telegram-desktop python python-pip wget nginx \
-openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light \
+openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip light \
 ttf-roboto playerctl papirus-icon-theme hwloc p7zip hsetroot \
 nemo firewalld tree man inter-font fzf mesa vulkan-radeon libva-mesa-driver \
 mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr xf86-video-vmware python-dbus discord bind-tools \
-python-pywal i3lock dbeaver ccache ttf-cascadia-code ttf-opensans httpie pavucontrol
+python-pywal i3lock dbeaver ccache ttf-cascadia-code ttf-opensans httpie pavucontrol nvidia nvidia-config
 
 # generating fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -132,12 +118,12 @@ arch-chroot /mnt locale-gen
 arch-chroot /mnt echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
 
 # setting machine name
-arch-chroot /mnt echo "mrczlnks" >> /mnt/etc/hostname
+arch-chroot /mnt echo "jacklnx" >> /mnt/etc/hostname
 
 # setting hosts file
 arch-chroot /mnt echo "127.0.0.1 localhost" >> /mnt/etc/hosts
 arch-chroot /mnt echo "::1 localhost" >> /mnt/etc/hosts
-arch-chroot /mnt echo "127.0.1.1 mrczlnks.localdomain mrczlnks" >> /mnt/etc/hosts
+arch-chroot /mnt echo "127.0.1.1 jacklnx.localdomain jacklnx" >> /mnt/etc/hosts
 
 # making sudoers do sudo stuff without requiring password typing
 arch-chroot /mnt sed -i -e 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
@@ -148,24 +134,24 @@ arch-chroot /mnt mkinitcpio -p linux
 # setting root password
 arch-chroot /mnt echo "root:$root_password" | chpasswd
 
-# making user mrcz
-arch-chroot /mnt useradd -m -G wheel -s /bin/zsh mrcz
+# making user jack
+arch-chroot /mnt useradd -m -G wheel -s /bin/zsh jack
 
-# setting mrcz password
-arch-chroot /mnt echo "mrcz:$user_password" | chpasswd
+# setting jack password
+arch-chroot /mnt echo "jack:$user_password" | chpasswd
 
 # installing systemd-boot
 arch-chroot /mnt bootctl --path=/boot install
 
-# configuring mrczlnks boot entry
+# configuring jacklnx boot entry
 arch-chroot /mnt /bin/zsh -c "grep \"UUID=\" /etc/fstab | grep '/ ' | awk '{ print \$1 }' | sed -e 's/UUID=//' > .root_disk_uuid"
-arch-chroot /mnt /bin/zsh -c 'touch /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'echo "title mrczlnks" >> /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'echo "linux /vmlinuz-linux" >> /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'echo "initrd /amd-ucode.img" >> /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'echo "initrd /initramfs-linux.img" >> /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'echo options root=\"UUID=root_disk_uuid\" rw >> /boot/loader/entries/mrczlnks.conf'
-arch-chroot /mnt /bin/zsh -c 'sed -i -e "s/root_disk_uuid/$(cat .root_disk_uuid)/g" /boot/loader/entries/mrczlnks.conf'
+arch-chroot /mnt /bin/zsh -c 'touch /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'echo "title jacklnx" >> /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'echo "linux /vmlinuz-linux" >> /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'echo "initrd /amd-ucode.img" >> /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'echo "initrd /initramfs-linux.img" >> /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'echo options root=\"UUID=root_disk_uuid\" rw >> /boot/loader/entries/jacklnx.conf'
+arch-chroot /mnt /bin/zsh -c 'sed -i -e "s/root_disk_uuid/$(cat .root_disk_uuid)/g" /boot/loader/entries/jacklnx.conf'
 arch-chroot /mnt /bin/zsh -c 'rm .root_disk_uuid'
 
 # changing governor to performance
@@ -183,61 +169,61 @@ arch-chroot /mnt systemctl enable fstrim.timer
 arch-chroot /mnt systemctl enable systemd-resolved.service
 arch-chroot /mnt systemctl start systemd-resolved.service
 
-# making i3 default for startx for both root and mrcz
+# making i3 default for startx for both root and jack
 arch-chroot /mnt echo "exec i3" >> /mnt/root/.xinitrc
-arch-chroot /mnt echo "exec i3" >> /mnt/home/mrcz/.xinitrc
+arch-chroot /mnt echo "exec i3" >> /mnt/home/jack/.xinitrc
 
 # installing yay
-arch-chroot /mnt sudo -u mrcz git clone https://aur.archlinux.org/yay.git /home/mrcz/yay_tmp_install
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "cd /home/mrcz/yay_tmp_install && yes | makepkg -si"
-arch-chroot /mnt rm -rf /home/mrcz/yay_tmp_install
+arch-chroot /mnt sudo -u jack git clone https://aur.archlinux.org/yay.git /home/jack/yay_tmp_install
+arch-chroot /mnt sudo -u jack /bin/zsh -c "cd /home/jack/yay_tmp_install && yes | makepkg -si"
+arch-chroot /mnt rm -rf /home/jack/yay_tmp_install
 
 # adding makepkg optimizations
 arch-chroot /mnt sed -i -e 's/#MAKEFLAGS="-j2"/MAKEFLAGS=-j'$(nproc --ignore 1)'/' -e 's/-march=x86-64 -mtune=generic/-march=native/' -e 's/xz -c -z/xz -c -z -T '$(nproc --ignore 1)'/' /etc/makepkg.conf
 arch-chroot /mnt sed -i -e 's/!ccache/ccache/g' /etc/makepkg.conf
 
 # installing various packages from AUR
-arch-chroot /mnt sudo -u mrcz yay -S i3-gaps --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S polybar --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S downgrade --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S spotify --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S corrupter-bin --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S whatsapp-nativefier-dark --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S simplenote-electron-bin --noconfirm
-arch-chroot /mnt sudo -u mrcz yay -S picom-tryone-git --noconfirm
+arch-chroot /mnt sudo -u jack yay -S i3-gaps --noconfirm
+arch-chroot /mnt sudo -u jack yay -S polybar --noconfirm
+arch-chroot /mnt sudo -u jack yay -S downgrade --noconfirm
+arch-chroot /mnt sudo -u jack yay -S spotify --noconfirm
+arch-chroot /mnt sudo -u jack yay -S corrupter-bin --noconfirm
+arch-chroot /mnt sudo -u jack yay -S whatsapp-nativefier-dark --noconfirm
+arch-chroot /mnt sudo -u jack yay -S simplenote-electron-bin --noconfirm
+arch-chroot /mnt sudo -u jack yay -S picom-tryone-git --noconfirm
 
 # installing oh-my-zsh
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c 'cd ~ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh && chmod +x install.sh && RUNZSH=no ./install.sh && rm ./install.sh'
+arch-chroot /mnt sudo -u jack /bin/zsh -c 'cd ~ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh && chmod +x install.sh && RUNZSH=no ./install.sh && rm ./install.sh'
 
 # installing pi theme for zsh
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "wget -O /home/mrcz/.oh-my-zsh/themes/pi.zsh-theme https://raw.githubusercontent.com/tobyjamesthomas/pi/master/pi.zsh-theme"
+arch-chroot /mnt sudo -u jack /bin/zsh -c "wget -O /home/jack/.oh-my-zsh/themes/pi.zsh-theme https://raw.githubusercontent.com/tobyjamesthomas/pi/master/pi.zsh-theme"
 
 # installing vundle
-arch-chroot /mnt sudo -u mrcz mkdir -p /home/mrcz/.config/nvim/bundle
-arch-chroot /mnt sudo -u mrcz git clone https://github.com/VundleVim/Vundle.vim.git /home/mrcz/.config/nvim/bundle/Vundle.vim
+arch-chroot /mnt sudo -u jack mkdir -p /home/jack/.config/nvim/bundle
+arch-chroot /mnt sudo -u jack git clone https://github.com/VundleVim/Vundle.vim.git /home/jack/.config/nvim/bundle/Vundle.vim
 
 # installing fonts
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/fonts_tmp_folder
-arch-chroot /mnt sudo -u mrcz sudo mkdir /usr/share/fonts/OTF/
+arch-chroot /mnt sudo -u jack mkdir /home/jack/fonts_tmp_folder
+arch-chroot /mnt sudo -u jack sudo mkdir /usr/share/fonts/OTF/
 # material icons
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "cd /home/mrcz/fonts_tmp_folder && wget -O materialicons.zip https://github.com/google/material-design-icons/releases/download/3.0.1/material-design-icons-3.0.1.zip && unzip materialicons.zip"
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "sudo cp /home/mrcz/fonts_tmp_folder/material-design-icons-3.0.1/iconfont/MaterialIcons-Regular.ttf /usr/share/fonts/TTF/"
+arch-chroot /mnt sudo -u jack /bin/zsh -c "cd /home/jack/fonts_tmp_folder && wget -O materialicons.zip https://github.com/google/material-design-icons/releases/download/3.0.1/material-design-icons-3.0.1.zip && unzip materialicons.zip"
+arch-chroot /mnt sudo -u jack /bin/zsh -c "sudo cp /home/jack/fonts_tmp_folder/material-design-icons-3.0.1/iconfont/MaterialIcons-Regular.ttf /usr/share/fonts/TTF/"
 # removing fonts tmp folder
-arch-chroot /mnt sudo -u mrcz rm -rf /home/mrcz/fonts_tmp_folder
+arch-chroot /mnt sudo -u jack rm -rf /home/jack/fonts_tmp_folder
 
 # installing config files
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/GitHub
-arch-chroot /mnt sudo -u mrcz git clone https://github.com/ilbuonmarcio/mrczlnks /home/mrcz/GitHub/mrczlnks
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "chmod 700 /home/mrcz/GitHub/mrczlnks/install_configs.sh"
-arch-chroot /mnt sudo -u mrcz /bin/zsh -c "cd /home/mrcz/GitHub/mrczlnks && ./install_configs.sh"
+arch-chroot /mnt sudo -u jack mkdir /home/jack/GitHub
+arch-chroot /mnt sudo -u jack git clone https://github.com/GiacomoTezza/jacklnx /home/jack/GitHub/jacklnx
+arch-chroot /mnt sudo -u jack /bin/zsh -c "chmod 700 /home/jack/GitHub/jacklnx/install_configs.sh"
+arch-chroot /mnt sudo -u jack /bin/zsh -c "cd /home/jack/GitHub/jacklnx && ./install_configs.sh"
 
 # create folder for screenshots
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/Screenshots
+arch-chroot /mnt sudo -u jack mkdir /home/jack/Screenshots
 
 # create pictures folder, secrets folder and moving default wallpaper
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/Pictures/
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/.secrets/
-arch-chroot /mnt sudo -u mrcz mkdir /home/mrcz/Pictures/wallpapers/
+arch-chroot /mnt sudo -u jack mkdir /home/jack/Pictures/
+arch-chroot /mnt sudo -u jack mkdir /home/jack/.secrets/
+arch-chroot /mnt sudo -u jack mkdir /home/jack/Pictures/wallpapers/
 
 # enable features on /etc/pacman.conf file
 arch-chroot /mnt sed -i -e 's/#UseSyslog/UseSyslog/g' /etc/pacman.conf
