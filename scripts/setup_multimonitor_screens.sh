94947a39bdef077ce7af73512764f2130b75f393
GiacomoTezza <tezza.giacomo01@gmail.com>
2020-05-02 14:38:29 +0200

    Fixed monitor settings

--- scripts/setup_multimonitor_screens.sh
+++ scripts/setup_multimonitor_screens.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144 --primary
-xrandr --output DisplayPort-2 --mode 2560x1440 --rate 75 --left-of DisplayPort-0
+xrandr --output DP-4 --mode 2560x1440 --rate 144 --primary
 
-xrandr --output DisplayPort-0 --set TearFree on
-xrandr --output DisplayPort-2 --set TearFree on
+xrandr --output DP-4 --set TearFree on
