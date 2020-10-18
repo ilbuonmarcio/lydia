1cda71f6f1f137015447039e35c44f2075709868
Alessandro Marchioro <marciozgaming@gmail.com>
2020-10-18 12:57:44 +0200

    changed default video ports

--- scripts/setup_multimonitor_screens.sh
+++ scripts/setup_multimonitor_screens.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-xrandr --output DisplayPort-2 --mode 2560x1440 --rate 144 --primary
-xrandr --output DisplayPort-2 --set TearFree on
+xrandr --output DP-0 --mode 2560x1440 --rate 144 --primary
+xrandr --output DP-0 --set TearFree on
