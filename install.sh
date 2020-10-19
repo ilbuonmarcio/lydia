f2e206a6131bc25bb61229cb53b45e5a11d348aa
Alessandro Marchioro <marciozgaming@gmail.com>
2020-10-19 20:57:21 +0200

    added FUCKING gnome keyring

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ pulseaudio pamixer telegram-desktop python python-pip wget nginx \
 openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip \
 ttf-roboto playerctl papirus-icon-theme hwloc p7zip hsetroot \
 nemo tree man inter-font fzf mesa vulkan-radeon libva-mesa-driver \
-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr python-dbus discord bind-tools \
+mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr python-dbus discord bind-tools gnome-keyring \
 i3lock dbeaver ccache ttf-cascadia-code ttf-opensans httpie pavucontrol docker docker-compose picom mpv iotop bspwm sxhkd gitg filelight
 
 # generating fstab
